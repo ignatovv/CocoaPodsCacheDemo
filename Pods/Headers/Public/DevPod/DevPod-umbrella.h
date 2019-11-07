51eda688ab8c1c32c8728e828d69a6e0bf7d2a92
Vladimir Ignatov <vyuignatov@avito.ru>
2019-11-07 17:49:37 +0300

    Remove pods directory from git

--- Pods/Headers/Public/DevPod/DevPod-umbrella.h
+++
@@ -1,1 +0,0 @@
-../../../Target Support Files/DevPod/DevPod-umbrella.h
--- Pods/Target Support Files/DevPod/DevPod-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DevPod : NSObject
-@end
-@implementation PodsDummy_DevPod
-@end
